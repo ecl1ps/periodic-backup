bc0bbec9a404cf6f253436f61f6b840e7f89acdf
Eclips <eclips@x.y>
2019-01-31 21:45:35 +0100

    Added functionality to backup multiple files to single archive

---
+++ PeriodicBackup/BackupPackage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace PeriodicBackup
+{
+    public class BackupPackage
+    {
+        private List<FileInfo> Files = new List<FileInfo>();
+
+        public void AddFile(FileInfo fileInfo)
+        {
+            Files.Add(fileInfo);
+        }
+
+        public void Pack(string outPathName, int compressionLevel)
+        {
+            var zipStream = new ZipOutputStream(File.Create(outPathName));
+
+            zipStream.SetLevel(compressionLevel); //0-9, 9 being the highest level of compression
+
+            CompressFiles(zipStream);
+
+            zipStream.IsStreamOwner = true; // Makes the Close also Close the underlying stream
+            zipStream.Close();
+        }
+
+        private void CompressFiles(ZipOutputStream zipStream)
+        {
+            foreach (var fi in Files)
+            {
+                var newEntry = new ZipEntry(fi.Name)
+                {
+                    DateTime = fi.LastWriteTime,
+                    Size = fi.Length
+                };
+
+                zipStream.PutNextEntry(newEntry);
+
+                byte[] buffer = new byte[4096];
+                using (FileStream streamReader = fi.OpenRead())
+                    StreamUtils.Copy(streamReader, zipStream, buffer);
+
+                zipStream.CloseEntry();
+            }
+        }
+    }
+}
--- PeriodicBackup/Program.cs
+++ PeriodicBackup/Program.cs
@@ -9,54 +9,78 @@ using System.Threading.Tasks;
 
 namespace PeriodicBackup
 {
+    class FileData
+    {
+        public string FilePath { get; }
+        public DateTime LastChange { get; set; }
+
+        public FileData(string filePath)
+        {
+            FilePath = filePath;
+        }
+    }
+
     class Program
     {
-        private static string originalFileName;
+        private static List<FileData> backupedFiles;
         private static string outputDirectory;
-        private static DateTime lastChange;
+        private static string outputFileName;
+        private static string outputExtension;
 
         static void Main(string[] args)
         {
             if (args.Length < 2)
             {
-                Console.WriteLine("PeriodicBackup <time in minutes> <file to backup>[ <directory to backup>]");
+                Console.WriteLine("PeriodicBackup <time in minutes> <files to backup separated by semicolon> [<directory to backup>] [<name of output file>]");
                 return;
             }
 
-            int minutes;
-            if (!int.TryParse(args[0], out minutes))
+            if (!int.TryParse(args[0], out int minutes))
             {
                 Console.WriteLine("Time '{0}' is not a valit number", args[0]);
                 return;
             }
 
-            if (!File.Exists(args[1]))
+            var files = args[1].Split(';');
+            if (files.Length == 0)
             {
-                Console.WriteLine("File '{0}' not found", args[1]);
+                Console.WriteLine("No files for backup specified");
                 return;
             }
 
-            originalFileName = args[1];
-            var originalFile = new FileInfo(originalFileName);
+            if (args.Length < 4 && files.Length > 1)
+            {
+                Console.WriteLine("Output file name must be specified when backing-up multiple files");
+                return;
+            }
+
+            backupedFiles = new List<FileData>();
+            foreach (var file in files)
+            {
+                if (!File.Exists(file))
+                {
+                    Console.WriteLine("File '{0}' not found", file);
+                    return;
+                }
+
+                backupedFiles.Add(new FileData(file));
+            }
+
+            outputFileName = args.Length == 4 ? args[3] : Path.GetFileNameWithoutExtension(backupedFiles.First().FilePath);
+            outputExtension = string.Empty;
 
             if (args.Length > 2)
             {
-                if (!Directory.Exists(args[2]))
-                {
-                    Console.WriteLine("Directory '{0}' not found", args[2]);
-                    return;                    
-                }
-                else
-                {
-                    outputDirectory = args[2];
-                }
+                outputDirectory = args[2];
             }
             else
             {
+                var originalFile = new FileInfo(backupedFiles.FirstOrDefault().FilePath);
                 outputDirectory = Path.Combine(originalFile.DirectoryName, "PeriodicBackup");
             }
 
-            Console.WriteLine("Periodic backups of '{0}' every {1} minutes to '{2}' started", originalFile.Name, minutes, outputDirectory);
+            Console.WriteLine("Started periodic backups every {0} minutes to '{1}' of files:", minutes, outputDirectory);
+            backupedFiles.ForEach(f => Console.WriteLine(new FileInfo(f.FilePath).Name));
             Console.WriteLine("------------------------------------------------------------------");
 
             AutoResetEvent autoEvent = new AutoResetEvent(false);
@@ -66,32 +90,29 @@ namespace PeriodicBackup
 
         static void Tick(object o)
         {
-            var originalFile = new FileInfo(originalFileName);
-            if (originalFile.LastWriteTime != lastChange)
+            var info = backupedFiles.Select(bf => new { BackupInfo = bf, FileInfo = new FileInfo(bf.FilePath) });
+            var now = DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss");
+
+            if (info.All(i => i.BackupInfo.LastChange >= i.FileInfo.LastWriteTime))
             {
-                Console.WriteLine("Creating backup in {0}", DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss"));
-
-                if (!Directory.Exists(outputDirectory))
-                    Directory.CreateDirectory(outputDirectory);
-
-                using (FileStream inFile = originalFile.OpenRead())
-                {
-                    using (FileStream outFile = File.Create(Path.Combine(outputDirectory,
-                        string.Format("{0}-{1}{2}.gz", Path.GetFileNameWithoutExtension(originalFile.Name), DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss"), originalFile.Extension))))
-                    {
-                        using (GZipStream compress = new GZipStream(outFile, CompressionMode.Compress))
-                        {
-                            inFile.CopyTo(compress);
-                        }
-                    }
-                }
-
-                lastChange = originalFile.LastWriteTime;
+                Console.WriteLine("Files has not changed, backup skipped in {0}", now);
+                return;
             }
-            else
-            {
-                Console.WriteLine("File has not changed, backup skipped in {0}", DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss"));
-            }
+
+            Console.WriteLine("Creating backup in {0}", now);
+
+            if (!Directory.Exists(outputDirectory))
+                Directory.CreateDirectory(outputDirectory);
+
+            var package = new BackupPackage();
+
+            foreach (var i in info)
+                package.AddFile(i.FileInfo);
+
+            package.Pack(Path.Combine(outputDirectory, string.Format("{0}-{1}{2}.gz", outputFileName, now, outputExtension)), 6);
+
+            foreach (var i in info)
+                i.BackupInfo.LastChange = i.FileInfo.LastWriteTime;
         }
     }
 }
